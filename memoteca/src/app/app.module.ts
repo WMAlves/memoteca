c1ae0eabb7978ded4f06163253050fcf90bcd2c9
WMAlves <williamma03@gmail.com>
2023-05-27 10:49:16 -0300

    Criaçao de componetes

--- memoteca/src/app/app.module.ts
+++ memoteca/src/app/app.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+import { RodapeComponent } from './components/rodape/rodape.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    
-    CabecalhoComponent
+
+    CabecalhoComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
